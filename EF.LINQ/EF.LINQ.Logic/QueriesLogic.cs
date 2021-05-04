eef0db9c1282961686e2747a95c37bd10939ee2d
nicursed <nicursed@gmail.com>
2021-05-04 01:29:58 -0300

    TP4 Entity Framework Linq

---
+++ EF.LINQ/EF.LINQ.Logic/QueriesLogic.cs
@@ -0,0 +1,92 @@
+﻿using EF.LINQ.Data;
+using EF.LINQ.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.LINQ.Logic
+{
+    public class QueriesLogic
+    {
+        private readonly NorthwindContext context;
+
+        public QueriesLogic()
+        {
+            context = new NorthwindContext();
+        }
+
+
+        public List<Customer> ObjetoCustomer()
+        {
+
+            var queryCustomer = from customers in context.Customers
+                                where customers.CompanyName == "Chop-suey Chinese"
+                                select customers;
+
+            return queryCustomer.ToList();
+        }
+
+        public List<Product> ProdSinStock()
+        {
+            var querySinStock = from products in context.Products
+                                where products.UnitsInStock == 0
+                                select products;
+
+            return querySinStock.ToList();
+
+        }
+
+        public List<Product> ProdEnStock()
+        {
+            var queryEnStock = from products in context.Products
+                               where products.UnitsInStock >= 1 && products.UnitPrice > 3
+                               select products;
+
+
+            return queryEnStock.ToList();
+        }
+
+        public List<Customer> CustWA()
+        {
+            var queryCustWA = from customers in context.Customers
+                              where customers.Region == "WA"
+                              select customers;
+
+            return queryCustWA.ToList();
+        }
+
+        public Product ProdFirstNull()
+        {
+            var queryFirstNull = context.Products.FirstOrDefault(p => p.ProductID == 789);
+
+            return queryFirstNull;
+        }
+
+        public List<Customer> CustName()
+        {
+            var custName = from customers in context.Customers
+                           select customers;
+
+            return custName.ToList();
+        }
+
+        public IQueryable JoinCustYOrders()
+        {
+            var date = new DateTime(1997, 1, 1);
+            var queryJoin = from customer in context.Customers
+                        join order in context.Orders
+                        on customer.CustomerID equals order.CustomerID
+                        where customer.Region == "WA" && order.OrderDate > date
+                        select new
+                        {
+                            customer.ContactName,
+                            order.OrderDate
+                        };
+
+            return queryJoin;
+        }
+
+    }
+}
---
+++ EF.LINQ/EF.LINQ.UI/Program.cs
@@ -0,0 +1,68 @@
+﻿using EF.LINQ.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.LINQ.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QueriesLogic query = new QueriesLogic();
+
+            Console.WriteLine("Presione cualquier tecla para pasar de consigna en consigna \n");
+            Console.ReadKey();
+
+            foreach (var item in query.ObjetoCustomer())
+            {
+                Console.WriteLine($"Punto 1 - Customer Company Name: {item.CompanyName}" + "\n");
+            }
+            Console.ReadKey();
+
+
+            Console.WriteLine("Punto 2 - Productos sin stock: \n");
+            foreach (var item in query.ProdSinStock())
+            {
+                Console.WriteLine(item.ProductName + "\n");
+            }
+            Console.ReadKey();
+
+
+            Console.WriteLine("Punto 3 - Productos en stock y Unit price: \n");
+            foreach (var item in query.ProdEnStock())
+            {
+                Console.WriteLine($"{item.UnitsInStock} - {item.UnitPrice}" + "\n");
+            }
+            Console.ReadKey();
+
+
+            Console.WriteLine("Punto 4 - Customers de Washington: \n");
+            foreach (var item in query.CustWA())
+            {
+                Console.WriteLine($"Nombre: {item.ContactName} - Nombre de la compania: {item.CompanyName}" + "\n");
+            }
+            Console.ReadKey();
+
+
+            Console.WriteLine("Punto 5 - ID Null \n");
+            var idNull = query.ProdFirstNull();
+
+            if (idNull == null) Console.WriteLine("Sin valor \n");
+            Console.ReadKey();
+
+
+            Console.WriteLine("Punto 6 - Nombres en May y Min: \n");
+            foreach (var item in query.CustName())
+            {
+                Console.WriteLine($"{item.ContactName.ToUpper()} - {item.ContactName.ToLower()}");
+            }
+
+           //Console.WriteLine("Punto 7 - Join Customers y Orders: \n"); - No supe que ni como mostrar
+
+            Console.ReadLine();
+        }
+    }
+}
